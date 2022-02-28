d08b677c677c9ef5038e85c00b8a81b3c287a920
ainga Ramangalahy <aingaramangalahy@gmail.com>
2022-02-28 20:59:13 +0300

    latest updates

--- src/service/service.schema.ts
+++ src/service/service.schema.ts
@@ -5,10 +5,7 @@ export const ServiceSchema = new Schema(
     id: Number,
     intitule: {
       type: String,
-      required: false,
       trim: true,
-      minlength: [2, 'Please use 2 or more characters'],
-      maxlength: [50, 'Max authorized characters is 50'],
     },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, required: false },
